aafc7458ade44e9d4e479fee0f1e14070f59f6e4
Donavan Duplessis <donavan.duplessis@outlook.com>
2024-12-02 21:46:09 +0000

    6th commit

---
+++ ts-5/day-1.js
@@ -0,0 +1,95 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+{
+    var transaction1 = {
+        description: 'Deposit',
+        amount: 2000,
+        date: new Date()
+    };
+    var first_user = {
+        name: "Donavan Duplessis",
+        age: 28,
+        logged_in: true
+    };
+    var second_user = {
+        name: "Ron Smith",
+        age: 24,
+        logged_in: false
+    };
+    console.log("This is the age of first user: ", first_user.age);
+    console.log("This is the age of second user: ", second_user.age);
+    var my_array = [first_user, second_user];
+    console.log("This is the array of user objects: ", my_array);
+    console.log("This is the first entry of user array: ", my_array[0]);
+    console.log("This is the name of the second entry of the user array ", my_array[1].name);
+    var user3 = __assign(__assign({}, first_user), { age: 24 });
+    var name_1 = user3.name, age = user3.age, logged_in = user3.logged_in;
+    var user4 = __assign(__assign({}, transaction1), { amount: 15 });
+    var description = user4.description, amount = user4.amount, date = user4.date;
+    console.log(description, amount, date);
+    // const transactionArea: HTMLDivElement = document.getElementById("transactions") as HTMLDivElement
+    // const el1 = `<li>${transaction1.date} ${transaction1.description} ${transaction1.amount}</li>`
+    // const el2 = `<li>${transaction1.date} ${transaction1.description} ${transaction1.amount}</li>`
+    // const ul = `${el1} ${el2}`
+    // const element1 = `<li>${first_user.name}</li>`
+    // const unorderedList = ``
+    var transactionArea_1 = document.getElementById("users");
+    var element1 = "<li>".concat(first_user.name, "</li>");
+    var element2 = "<p>User is not logged in</p>";
+    var ul = "<ul>".concat(element1, "</ul>");
+    // if (first_user.logged_in === true) {
+    //     transactionArea.innerHTML = ul
+    // } else {
+    //     transactionArea.innerHTML = element2
+    // }
+    //
+    function sayHello() {
+        console.log("Hello");
+    }
+    sayHello();
+    var sayHelloArrow = function () {
+        console.log("HelloArrow");
+    };
+    sayHelloArrow();
+    var balance_1 = 0;
+    var processTransaction = function (transaction) {
+        var newBalance = balance_1 + transaction.amount;
+        console.log("Old balance was ".concat(balance_1, ". New balance is ").concat(newBalance));
+        return newBalance;
+    };
+    var loginUser = function (first_user) {
+        first_user.logged_in = true;
+        transactionArea_1.innerHTML = "<p>Welcome ".concat(first_user.name, "</p>");
+    };
+    //
+    var exampleArray = ['sfa', 'fafa', 'atea'];
+    exampleArray.push('aa');
+    var name1 = exampleArray[0], name2 = exampleArray[1];
+    var exampleArrayCopy = __spreadArray(__spreadArray([], exampleArray, true), ['test', 'hi'], false);
+    //
+    // myinput: HTMLInputElement = document.getElementById("transactions") as HTMLInputElement
+    var transactionArray_1 = [123, 567];
+    var newTransaction = function (input) {
+        var myinput = document.getElementById("transactions");
+        transactionArray_1.push(Number(myinput.value));
+        console.log(transactionArray_1);
+    };
+}
---
+++ ts-5/day-1.ts
@@ -0,0 +1,162 @@
+{
+    // console.log("Hello Tampa!!!!!")
+
+    // const balance:number = 100
+    // const account = "ABC123"
+    // console.log(typeof balance);
+
+    // let mutableBalance:number = 254000
+
+    // let deposit: number = Number(prompt("What is your deposit amount???"))
+
+    // mutableBalance += deposit
+    // console.log("Your new balance is:", mutableBalance)
+
+
+    // let my_number:number = 200
+    // let my_string:string = "hello"
+    // let myBoolean:boolean = true
+
+    // console.log(typeof my_number)
+    // console.log(typeof my_string)
+    // console.log(typeof myBoolean)
+
+    // let greeting:string = "hello"
+    // let name:string = String(prompt("Hi there, please enter your name."))
+
+    // console.log(`${greeting} ${name}. This is a test message.`)
+
+    // let transation = {
+    //     description: "Deposit",
+    //     amount: 20000,
+    //     date: new Date()
+    // }
+
+    type TransactionType = {
+        description?: string,
+        amount: number,
+        date: Date
+    }
+
+    const transaction1: TransactionType = {
+        description: 'Deposit',
+        amount: 2000,
+        date: new Date()
+    }
+
+    // const transaction2: TransactionType = {
+    //     description: 'Deposit',
+    //     amount: 2000,
+    //     date: new Date()
+    // }
+
+    // const constdemoArray = [1, 'hello', 3, console.log]
+
+    // const numberArray: (number|string)[] = ['hello', 123]
+
+    type user = {
+        name: String,
+        age: Number,
+        logged_in: Boolean
+    }
+
+    let first_user: user = {
+        name: "Donavan Duplessis",
+        age: 28,
+        logged_in: true
+    }
+
+    let second_user: user = {
+        name: "Ron Smith",
+        age: 24,
+        logged_in: false
+    }
+
+    console.log("This is the age of first user: ", first_user.age)
+    console.log("This is the age of second user: ", second_user.age)
+
+    let my_array:user[] = [first_user, second_user]
+    console.log("This is the array of user objects: ", my_array)
+
+    console.log("This is the first entry of user array: ", my_array[0])
+    console.log("This is the name of the second entry of the user array ", my_array[1].name)
+
+
+    const user3:user = {...first_user, age: 24}
+    const {name, age, logged_in} = user3
+
+    const user4:TransactionType = {...transaction1, amount: 15}
+    const {description, amount, date} = user4
+
+    console.log(description, amount, date)
+
+    // const transactionArea: HTMLDivElement = document.getElementById("transactions") as HTMLDivElement
+
+    // const el1 = `<li>${transaction1.date} ${transaction1.description} ${transaction1.amount}</li>`
+    // const el2 = `<li>${transaction1.date} ${transaction1.description} ${transaction1.amount}</li>`
+
+    // const ul = `${el1} ${el2}`
+
+    // const element1 = `<li>${first_user.name}</li>`
+    // const unorderedList = ``
+
+    const transactionArea: HTMLDivElement = document.getElementById("users") as HTMLDivElement
+
+    const element1 = `<li>${first_user.name}</li>`
+    const element2 = `<p>User is not logged in</p>`
+
+    const ul = `<ul>${element1}</ul>`
+    
+    // if (first_user.logged_in === true) {
+    //     transactionArea.innerHTML = ul
+    // } else {
+    //     transactionArea.innerHTML = element2
+    // }
+
+    //
+    function sayHello(): void {
+        console.log("Hello")
+    }
+
+    sayHello()
+
+    const sayHelloArrow = ():void => {
+        console.log("HelloArrow")
+    }
+
+    sayHelloArrow()
+
+    const balance:number= 0 
+
+    const processTransaction = (transaction: TransactionType) : number => {
+        const newBalance = balance + transaction.amount
+        console.log(`Old balance was ${balance}. New balance is ${newBalance}`)
+        return newBalance
+    }
+
+    const loginUser = (first_user) => {
+        first_user.logged_in = true
+        transactionArea.innerHTML = `<p>Welcome ${first_user.name}</p>`
+    }
+
+    //
+
+    const exampleArray: string[] = ['sfa', 'fafa', 'atea']
+    exampleArray.push('aa')
+
+    const [name1, name2] = exampleArray
+
+    const exampleArrayCopy = [...exampleArray, 'test', 'hi']
+
+    //
+
+    // myinput: HTMLInputElement = document.getElementById("transactions") as HTMLInputElement
+    const transactionArray: Number[] = [123, 567]
+
+    const newTransaction = (input) => {
+        const myinput: HTMLInputElement = document.getElementById("transactions") as HTMLInputElement
+        transactionArray.push(Number(myinput.value))
+        console.log(transactionArray)
+    }
+
+}
